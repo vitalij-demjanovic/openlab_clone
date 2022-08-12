3909173ec19a7d0675dc52275b01630ba92e1167
vitalij.demjanovic <vitalidemjanovic@gmail.com>
2022-08-12 22:29:08 +0200

    start project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-button>
+      Vitalij
+    </b-button>
+  </div>
+</template>
+
+<script>
+
+import { BButton } from 'bootstrap-vue-3'
+export default {
+  name: 'App',
+  components: { BButton }
+}
+</script>
+
+<style>
+</style>
