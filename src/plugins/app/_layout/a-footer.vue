470bb3b0fb7015f2c5b90cdc8f12d935a2cae877
vitalij.demjanovic <vitalidemjanovic@gmail.com>
2022-08-24 21:16:52 +0200

    úprava footer

--- src/plugins/app/_layout/a-footer.vue
+++ src/plugins/app/_layout/a-footer.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
-    <b-row>
-      <b-col>
-        <img src="../_layout/_icons/openlab-logo.svg" alt="openlab">
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col class="col-3">
-        © 2022 Všetky práva vyhradené OPENLAB, o.z.,
-      </b-col>
-      <b-col class="col-3">
-        <p>IČO: 50764675</p>
-        <p>Viedenská cesta 257, 805 01 Bratislava</p>
-      </b-col>
-      <b-col class="col-3">
-        <a href="mailto:info@openlab.sk">info@openlab.sk</a>
-      </b-col>
-      <b-col class="col-3">
-        <img src="../_layout/_icons/facebook.svg" alt="facebook">
-        <img src="../_layout/_icons/linkedin.svg" alt="linkedin">
-        <img src="../_layout/_icons/instagram.svg" alt="instagram">
-      </b-col>
-    </b-row>
+  <div class="py-5">
+    <footer class="container text-gray position-relative">
+      <b-row class="align-items-end">
+        <b-col class="col-4">
+          <img src="../_layout/_icons/openlab-logo.svg" alt="openlab">
+          <p class="mt-2 mb-0">
+            © 2022 Všetky práva vyhradené
+            <span class="fw-bold">OPENLAB, o.z.,</span>
+          </p>
+        </b-col>
+        <b-col class="col-3">
+          <p class="mb-0">IČO: 50764675</p>
+          <p class="mb-0">Viedenská cesta 257, 805 01 Bratislava</p>
+        </b-col>
+        <b-col class="col-2">
+          <a class="text-gray text-decoration-underline" href="mailto:info@openlab.sk">info@openlab.sk</a>
+        </b-col>
+        <b-col class="col-3 d-flex footer-social">
+          <img src="../_layout/_icons/facebook.svg" alt="facebook">
+          <img src="../_layout/_icons/linkedin.svg" alt="linkedin">
+          <img src="../_layout/_icons/instagram.svg" alt="instagram">
+        </b-col>
+      </b-row>
+      <div class="footer-cube"/>
+    </footer>
   </div>
 </template>
 
@@ -33,6 +35,18 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="sass">
+footer
+  font-size: 13px !important
 
+.footer-social
+  justify-content: flex-end
+
+.footer-cube
+  background: #faf15b
+  width: 125px
+  height: 80px
+  position: absolute
+  left: 100%
+  top: calc(100% - 30px)
 </style>
