6c88bbc5e04af7fddc808d911a0c4c686622e9af
vitalij.demjanovic <vitalidemjanovic@gmail.com>
2022-09-07 23:59:44 +0200

    data slider edit school

--- src/plugins/app@hybridlab/_components/a-hybrid-slider.vue
+++ src/plugins/app@hybridlab/_components/a-hybrid-slider.vue
@@ -1,18 +1,85 @@
 <template>
-  <div>
-    auto
-    <button>One</button>
-    <button>two</button>
+  <div class="container pd-wrapper">
+
+    <b-row class="justify-content-end">
+      <b-col class="col-md-6 col-xl-5">
+        <h1 class="fw-light slider-title position-relative">
+          Projekty, ktorých súčasťou<br>
+          <span class="fw-bold">môžeš byť aj ty!</span>
+        </h1>
+      </b-col>
+    </b-row>
+    <div>
+      <div>
+        <b-row>
+          <b-col class="col-md-6 align-self-center">
+            <img src="../../app@hybridlab/_icons/financie-project.jpeg">
+          </b-col>
+          <b-col class="col-md-5 col-xl-5 mb-5 offset-xl-1 project-content">
+            <h2>Aplikácia Financie</h2>
+            <p class="text-gray">
+              Financie.sk je open source online platforma, ktorá slúži na vzdelávanie študentov v oblasti finančného sveta, a tým má ambíciu prispieť aj k zvýšeniu finančnej gramotnosti na Slovensku.
+              Platforma je určená pre učiteľov, študentov, rodičov a spotrebiteľov finančných služieb.<br>
+              Táto platforma má pomôcť učiteľom ľahšie naučiť žiakov danú problematiku, tým , že učiteľ príde na
+              stránku, stiahne si a vytlačí príručku pre seba a následne stiahne príručku aj pre žiakov, v ktorej sú už úlohy.<br>
+              Na vývoj študenti použili Vue, October CMS a Wordpress. Na tomto projekte pracovali študenti s klientom FInax a aktuálne je stránka vo fáze produkcie.
+            </p>
+          </b-col>
+        </b-row>
+      </div>
+      <div>
+        <b-row>
+          <b-col class="col-md-6 align-self-center">
+            <img src="../../app@hybridlab/_icons/academyapp-project.jpeg">
+          </b-col>
+          <b-col class="col-md-5 col-xl-5 mb-5 offset-xl-1 project-content">
+            <h2 class="fw-bold">OpenAcademy App</h2>
+            <p class="text-gray">
+              OpenAcademy je vzdelávacia webová platforma pre najnovšie technologické smery, ktorým sa venujeme v OpenLabe.
+              V tomto projekte študenti pracovali na vytvorení mobilnej aplikácie OpenAcademy platformy.
+              Študenti mali zámer vytvoriť aplikáciu jednoduchú a prehľadnú na používanie a sprístupniť ju pre mladších spolužiakov.
+              Cieľom aplikácie je naučiť študenta pomocou kvalitne vybraných materiálov, aby finálny projekt dobre zvládol.
+              Študenti pri vývoji použili na frontend Vue, kostru aplikácie stylovali v Ionic a dizajn robili pomocou Sass.
+              Mobilná aplikácia je funkčná a môžete si ju stiahnuť tu. Do budúcnosti plánujú študenti pokračovať v zlepšovaní aplikácie.
+            </p>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
+    <b-row>
+      <b-col >
+        <button class="slide-btn">
+          <z-icon iconName="slide-left"/>
+        </button>
+        <button class="slide-btn">
+          <z-icon iconName="slide-right"/>
+        </button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 
+import { BCol, BRow } from 'bootstrap-vue-3'
+import ZIcon from '@/plugins/app/_components/z-icon/z-icon'
 export default {
-  name: 'a-hybrid-slider'
+  name: 'a-hybrid-slider',
+  components: { ZIcon, BCol, BRow }
 }
 </script>
 
-<style scoped>
+<style lang="sass">
+.slider-title
+  &:before
+    content: ''
+    position: absolute
+    bottom: -25px
+    left: 0
+    width: 50px
+    height: 1px
+    background-color: rgba(86, 107, 141, 0.5)
 
+.project-content
+  margin-top: 120px !important
 </style>
--- src/plugins/app@school/_components/a-episodes.vue
+++ src/plugins/app@school/_components/a-episodes.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    <h2>Vypočuj si úvodné epizódy</h2>
-    <b-row>
+  <div class="container pd-wrapper">
+    <h2 class="text-center mb-5 fw-light">
+      <span class="fw-bold">Vypočuj si </span>
+      úvodné epizódy
+    </h2>
+    <b-row class="mt-5">
       <b-col class="col-6">
-        <div>VIDEO</div>
+        <div>
+          <figure class="position-relative">
+            <img src="../../app@school/_icons/episode2.jpeg">
+            <img
+              class="position-absolute start-50 top-50 translate-middle play-btn"
+              src="../../app/_icons/play.svg"
+              @click="modalShow = !modalShow"
+            >
+            <b-modal v-model="modalShow" class="p-0" id="modal-xl" size="xl">
+              <iframe width="100%" height="666" src="https://www.youtube.com/embed/tv-5-Rnr_qQ" title="Unikátny učebný program OpenLab očami študentov #06" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </b-modal>
+          </figure>
+        </div>
         <h5>Unikátny učebný program OpenLab očami študentov #06</h5>
-        <p>
+        <p class="text-gray">
           V dnešnej epizóde boli hosťami študentka Dominika a absolvent Marek, ktorí porozprávali o tom aké to je študovať v OpenLabe.
           Opísali rozdiely klasického študijného odboru na strednej škole a odboru kde pôsobí OpenLab, ako sa dostali k svojej prvej práci, či mnohé ďalšie skúsenosti.
           Absolventi OpenLabu sú častokrát lepšie pripravení ako ich rovesníci.
@@ -13,9 +28,30 @@
           S podporou LabMastra študenti pracujú na reálnych projektoch v tímoch, čím získavajú mäkké a digitálne zručnosti pre 21. storočie.
         </p>
       </b-col>
+      <b-col class="col-6">
+        <div>
+          <figure class="position-relative">
+            <img src="../../app@school/_icons/episode1.jpeg">
+            <img
+              class="position-absolute start-50 top-50 translate-middle play-btn"
+              src="../../app/_icons/play.svg"
+              @click="modalShowSecond = !modalShowSecond"
+            >
+            <b-modal v-model="modalShowSecond" class="p-0 w-100" id="modal-xl" size="xl">
+              <iframe width="100%" height="666" src="https://www.youtube.com/embed/eavR8DIIykM" title="Spúšťame aplikáciu na podporu herných vývojárov (podcast Moderná škola #02)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </b-modal>
+          </figure>
+        </div>
+        <h5>Spúšťame aplikáciu na podporu herných vývojárov (podcast Moderná škola #0</h5>
+        <p class="text-gray">
+          V najnovšej časti podcastu Moderná škola sa Yablko spolu s Grishim rozprávali o tom, že spúšťame novú aplikáciu GameChangers na podporu herných vývojárov.
+          Rozoberali otázky ako pre koho je určená aplikácia, čo prinesie hernému developerovi a patrónovi,
+          prečo práve GameChangers, ako to môže pomôcť herným vývojárom a mnoho dalších zaujímavých otázok a odpo
+        </p>
+      </b-col>
     </b-row>
     <div>
-      <button>
+      <button class="btn-lg g-btn mt-3">
         Prejsť sa Youtube
         <img src="../_icons/youtube.svg" alt="youtube">
       </button>
@@ -24,14 +60,33 @@
 </template>
 
 <script>
-import { BRow, BCol } from 'bootstrap-vue-3'
+import { BRow, BCol, BModal } from 'bootstrap-vue-3'
 
 export default {
   name: 'episodes',
-  components: { BRow, BCol }
+  components: { BModal, BRow, BCol },
+  data () {
+    return {
+      modalShow: false,
+      modalShowSecond: false
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="sass">
+.play-btn
+  cursor: pointer
+
+.modal-content
+  background-color: transparent !important
+  border: none !important
+
+.modal-body
+  border-radius: 10px !important
+
+.modal-footer,
+.modal-header
+  display: none !important
 
 </style>
