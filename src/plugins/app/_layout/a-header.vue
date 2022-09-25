b1357d6bf04c46957fd79c793de6e0c2db5b7111
vitalij.demjanovic <vitalidemjanovic@gmail.com>
2022-09-25 22:22:07 +0200

    mobile menu

--- src/plugins/app/_layout/a-header.vue
+++ src/plugins/app/_layout/a-header.vue
@@ -50,11 +50,80 @@
       </b-col>
       <b-col class="col-lg-3 d-flex justify-content-end">
         <button class="g-btn d-none d-lg-block">Pridajte sa k nám</button>
-        <button class="mobile-btn d-block d-lg-none">
+        <button
+          class="mobile-btn d-block d-lg-none"
+          @click="toggleShow"
+        >
           <z-icon icon-name="list"/>
         </button>
       </b-col>
     </b-row>
+    <div :class="['mobile-menu', { show }]">
+      <button
+        class="d-flex justify-content-end m-2"
+        @click="toggleShow"
+      >
+        <z-icon icon-name="close"/>
+      </button>
+      <ul class="nav-links mt-5 text-center">
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/">Úvod</router-link>
+        </li>
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/hybridlab">HybridLab</router-link>
+        </li>
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/gameslab">GamesLab</router-link>
+        </li>
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/applab">AppsLab</router-link>
+        </li>
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/pre-ucitelov">Pre učiteľov</router-link>
+        </li>
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/partneri">Partneri</router-link>
+        </li>
+        <li
+          class="link mb-4"
+          @click="toggleShow"
+        >
+          <router-link to="/moderna-skola">Moderná škola</router-link>
+        </li>
+      </ul>
+      <div class="p-2 d-flex justify-content-center">
+        <button class="btn-lg g-btn">Pridajte sa k nám</button>
+      </div>
+      <div class="p-3 m-auto">
+        <p class="mb-2 px-4 lh-1 text-center">Ak máš záujem študovať spolu s nami, sme tu pre teba…</p>
+        <div class="discord-community m-0 mt-3 d-flex align-items-center justify-content-center fs-6">
+          <p class="mb-0">OPENLAB COMMUNITY</p>
+          <z-icon class="space-ma" iconName="discord"/>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="['overlay', { show }]"
+      @click="toggleShow"
+    />
   </div>
 </template>
 
@@ -63,7 +132,17 @@ import { BRow, BCol } from 'bootstrap-vue-3'
 import ZIcon from '@/plugins/app/_components/z-icon/z-icon'
 export default {
   name: 'Navigation',
-  components: { ZIcon, BCol, BRow }
+  components: { ZIcon, BCol, BRow },
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggleShow () {
+      this.show = !this.show
+    }
+  }
 }
 </script>
 
@@ -124,5 +203,37 @@ export default {
 
 .submenu-item
   padding: 0.25rem 1rem !important
+
+.mobile-menu
+  display: flex
+  flex-direction: column
+  position: absolute
+  top: 0
+  right: -280px
+  z-index: 99
+  width: 0
+  min-height: 100vh
+  -webkit-transition: right 0.3s, width 0s
+  transition: right 0.3s, width 0s
+  background: #ffffff
+  &.show
+    width: 280px
+    right: 0
+  a
+    font-size: 16px !important
+
+.overlay
+  display: none
+  background-color: rgba(0, 0, 0, 0.6)
+  bottom: 0
+  left: 0
+  position: fixed
+  right: 0
+  top: 0
+  z-index: 49
+  -webkit-animation: fadeIn 1s
+  animation: fadeIn 1s
+  &.show
+    display: block
 
 </style>
